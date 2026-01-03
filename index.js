6355b770fc117efa826bcb2b2b2329ec2ff09332
gabogara <gabo1gara@gmail.com>
2026-01-03 00:20:44 -0800

    add solution to challenge 6 step 1

--- index.js
+++ index.js
@@ -138,6 +138,9 @@ allBtn.addEventListener("click", showAllGames);
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
+const unfundedGamesCount = GAMES_JSON.filter(
+  (game) => game.pledged < game.goal
+).length;
 
 // create a string that explains the number of unfunded games using the ternary operator
 
